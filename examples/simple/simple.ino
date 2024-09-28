d228d2187290848af37c4ed062f857ed3d330084
wahyu widodo <87890825+widodo-zone-cv2@users.noreply.github.com>
2024-09-28 22:09:26 +0700

    Create simple.ino

---
+++ examples/simple/simple.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include "PCF8574.h"
+
+PCF8574 pcf(0x20);  // Inisialisasi dengan alamat I2C 0x20
+
+void setup() {
+  pcf.begin();
+  
+  // Atur P0 sebagai output untuk mengontrol LED
+  pcf.pinMode(0, OUTPUT);
+
+  // Atur P1 sebagai input untuk membaca tombol
+  pcf.pinMode(1, INPUT);
+}
+
+void loop() {
+  // Membaca status tombol
+  bool buttonState = pcf.digitalRead(1);
+  
+  // Nyalakan LED jika tombol ditekan
+  if (buttonState == LOW) {  // Mengasumsikan tombol terhubung ke ground (aktif LOW)
+    pcf.digitalWrite(0, HIGH); // Nyalakan LED
+  } else {
+    pcf.digitalWrite(0, LOW);  // Matikan LED
+  }
+}
